8e4a5e87f69d7fd8956337b1335b7d8df183a9e2
jaafar <jaafar.tour.travel@gmail.com>
2024-02-20 10:17:24 +0100

    Your commit message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ReactWhatsappButton from "react-whatsapp-button";
 import Infotrip from "./pages/infotrip";
-import ContactForm from "./pages/conta";
+
 function App() {
   return (
     <div >
